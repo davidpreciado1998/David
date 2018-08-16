532c906babf5aa546de7ab56bbbe6c3f65862fc8
david arturo preciado osuna <arturo_preciado@live.com.mx>
2018-08-16 08:33:43 -0700

    En este cambio hicimos una clase con constructores

--- PracticaObjetos/Program.cs
+++ PracticaObjetos/Program.cs
@@ -7,24 +7,27 @@ using System.Threading.Tasks;
 namespace PrácticaObjetos {
     class Program {
         static void Main(string[] args) {
-            Persona persona1 = new Persona();
-            persona1.nombre = "Juan";
-            persona1.apellidos = "Perez";
-            persona1.edad = 20;
+            Contacto contacto1 = new Contacto();
 
-            Console.WriteLine(persona1.nombre + " " + persona1.apellidos + " tiene " + persona1.edad + " años");
+            contacto1.Nombre = "Pedro Lopez";
+            contacto1.TelefonoCasa = "(644) 115 - 2514";
+            contacto1.Direccion = "Guerrero 456";
 
-            persona1.cumplirAños();
+            Console.WriteLine("Nombre: " + contacto1.Nombre);
+            Console.WriteLine("Telefono de casa" + contacto1.TelefonoCasa);
 
-            Console.WriteLine("Ahora " + persona1.nombre + " tiene " + persona1.edad + " años");
+            Contacto contacto2 = new Contacto("Juan Perez");
+            Console.WriteLine("Contacto 2");
+            Console.WriteLine("Nombre: " + contacto2.Nombre);
 
-            persona1.altura = 1.5f;
+            Contacto contacto3 = new Contacto("Maria Navarro", "1234567890", "9876543210", "5 de febrero 895", "maria@correo.com");
+            Console.WriteLine("Contacto 3");
+            Console.WriteLine("Nombre: "+contacto3.Nombre);
+            Console.WriteLine("Celular: "+contacto3.TelefonoCelular);
+            Console.WriteLine("Casa: "+contacto3.TelefonoCasa);
+            Console.WriteLine("Direccion: "+contacto3.Direccion);
+            Console.WriteLine("Correo: "+contacto3.CorreoElectronico);
 
-            Console.WriteLine(persona1.nombre + " mide " + persona1.altura);
-
-            persona1.crecer(0.12f);
-
-            Console.WriteLine(persona1.nombre + " hora mide " + persona1.altura);
             Console.ReadLine();
         }
     }
